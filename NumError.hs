b0120c9896d0c6748759e9f34217fe4d82699167
Vicente Reyes <vreyesvaldivieso@gmail.com>
2018-09-27 23:12:16 -0300

    haskell file for simple error propagation

---
+++ NumError.hs
@@ -0,0 +1,29 @@
+module NumError
+  ( NumError (..)
+  , scalar
+  ) where
+
+mul (v1, e1) (v2, e2) = (v1*v2, v1*v2*sqrt ((e1/v1)^2 + (e2/v2)^2))
+suma (v1, e1) (v2, e2) = (v1+v2, sqrt (e1^2 + e2^2))
+resta (v1, e1) (v2, e2) = (v1-v2, sqrt (e1^2 + e2^2))
+divi (v1, e1) (v2, e2) = (v1/v2, v1/v2*sqrt ((e1/v1)^2 + (e2/v2)^2))
+
+newtype NumError a = E (a, a)
+  deriving (Eq, Ord)
+
+instance (Floating a) => Num (NumError a) where
+  E x + E y = E $ x `suma` y
+  E x - E y = E $ x `resta` y
+  E x * E y = E $ x `mul` y
+  abs (E (v, e)) = E (abs v, e)
+  signum (E (v, e)) = E (signum v, e)
+  fromInteger i = E (fromInteger i, 0)
+
+instance (Floating a) => Fractional (NumError a) where
+  E x / E y = E $ x `divi` y
+  fromRational r = E (fromRational r, 0)
+
+instance (Show a) => Show (NumError a) where
+  show (E a) = show a
+
+scalar x = E (x, 0)
